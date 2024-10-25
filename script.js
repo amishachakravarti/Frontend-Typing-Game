bd836a5effe845f6d0fd534fc920523336621825
Amisha Chakravarti <chakravartiamisha@gmail.com>
2024-10-25 23:34:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -115,7 +115,7 @@ function loadParagraph() {
         "You are the most important person in your whole life.",
         "Always be true to who you are, and ignore what other people have to say about you.",
         "Only demonstrate your strength when it’s really required.",
-        "Subscribe to Drop X Out"
+        "Subscribe to my youtube channel"
     ];
 
     const randomIndex = Math.floor(Math.random() * paragraph.length);
